03ebd1b213f70e78b188df8c67dbe781ace913b5
Raf <rmalenta@wp.pl>
2021-08-19 10:51:14 +0200

    prepared environment for unit tests and created few

---
+++ tests/Service/JWTServiceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Services\JWTService;
+use PHPUnit\Framework\TestCase;
+
+class JWTServiceTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function testItCreatesValidToken()
+    {
+        $jwt = new JWTService();
+        $token = $jwt->generateToken("Testowanko");
+
+        $this->assertSame("Testowanko", $jwt->getToken($token));
+    }
+    public function testTokenExpiresAfter600Seconds()
+    {
+        $jwt = new JWTService();
+        $token = $jwt->generateToken("Testowanko");
+        $jwt->addTime(6000);
+        $jwt->verifyToken($token);
+        $this->assertSame(false, $jwt->verifyToken($token));
+    }
+}
